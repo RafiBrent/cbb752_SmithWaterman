cf100c1cfeed557010b211858d9a6a0fa912e508
RafiBrent <105883594+RafiBrent@users.noreply.github.com>
2023-03-01 12:35:19 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+    name='SmithWaterman',
+    version='0.1.0',
+    packages=find_packages(include=['cbb752_WatermanSmith', 'SmithWaterman.*']),
+    install_requires=[
+        'numpy>=1.24.1',
+    ]
+)
