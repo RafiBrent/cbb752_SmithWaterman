66b6ce8a5579e894d4f7459b21f5e2493cee1fbf
RafiBrent <105883594+RafiBrent@users.noreply.github.com>
2023-03-01 13:08:52 -0600

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='SmithWaterman',
-    version='0.1.0',
-    packages=find_packages(include=['cbb752_WatermanSmith', 'SmithWaterman.*']),
-)
