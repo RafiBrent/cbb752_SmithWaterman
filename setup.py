da12de34f35a1bb255eac7422da4f8d20d52f155
RafiBrent <105883594+RafiBrent@users.noreply.github.com>
2023-03-01 12:43:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,4 @@ setup(
     name='SmithWaterman',
     version='0.1.0',
     packages=find_packages(include=['cbb752_WatermanSmith', 'SmithWaterman.*']),
-    install_requires=[
-        'numpy>=1.24.1',
-    ]
 )
